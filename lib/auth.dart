274367fbd7be71e9f3b7c75ceed5a48b701ee42e
Prateek Thakur <prateekthakur272@gmail.com>
2023-03-12 17:34:52 +0530

    adds circular progress indicator on sign in and sign up

--- lib/auth.dart
+++ lib/auth.dart
@@ -2,33 +2,61 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Auth{
-  static final user = FirebaseAuth.instance.currentUser;
+  static User? get user{
+    return FirebaseAuth.instance.currentUser;
+  }
+
   static signInWithEmailPassword(BuildContext context,email,password) async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+    );
     await FirebaseAuth.instance
         .signInWithEmailAndPassword(
         email: email, password: password)
         .then(
             (value){
-          ScaffoldMessenger.of(context)
-              .showSnackBar(const SnackBar(
-            content: Text("Signed In"),
-            backgroundColor: Colors.green,
-          ));
+              Navigator.pop(context);
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(const SnackBar(
+                content: Text("Signed In"),
+                backgroundColor: Colors.green,
+              ));
         }).onError((error, stackTrace){
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text((error as FirebaseAuthException).code),backgroundColor: Colors.red,)
-          );
+              Navigator.pop(context);
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text((error as FirebaseAuthException).code),backgroundColor: Colors.red,)
+              );
     });
   }
+
   static createAccountWithEmailPassword(BuildContext context,String name,String email,String password) async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+    );
     await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password)
         .then((value)async{
+          Navigator.pop(context);
           ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text("Account created successfully"),backgroundColor: Colors.green,)
+              const SnackBar(content: Text("Account created successfully, please verify email to sign in"),backgroundColor: Colors.green,)
           );
-      await value.user?.updateDisplayName(name);
-      value.user?.sendEmailVerification();
+          await value.user?.updateDisplayName(name).then((v){
+            value.user?.updateDisplayName(name);
+            Navigator.pop(context);
+          });
     }).onError((error, stackTrace){
+      Navigator.pop(context);
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text((error as FirebaseAuthException).code),backgroundColor: Colors.red,)
       );
@@ -48,7 +76,8 @@ class Auth{
                     const SnackBar(
                       content: Text("Signed out successfully"),
                     )
-                )
+                ),
+                Navigator.popUntil(context,ModalRoute.withName('/'))
               });
             }, child: const Text("Yes")),
             TextButton(onPressed: (){
